f8e90c02f40661ad3a3b845fa41f4fe1409bf4df
nikolovskipetar <petarnikolovski@hotmail.com>
2020-03-12 02:33:30 +0100

    added navbar, banner and features section

---
+++ assets/js/navbar.js
@@ -0,0 +1,30 @@
+$(window).scroll(function() {
+    let navigation = $('nav')
+    let border = $('#nav-border')
+
+    navigation.toggleClass('scrolled', $(this).scrollTop() > 50)
+
+    if (navigation.hasClass('scrolled')) {
+        border.css('border-bottom', 'unset')
+    } else {
+        border.css('border-bottom', '1px solid rgba(250, 250, 250, 0.1)')
+    }
+});
+
+let btn = $('#mobile-menu')
+let bars = $('#mob-menu')
+
+$('body').on('click', btn, function() {
+
+    setTimeout(function() {
+        if (btn.hasClass('collapsed')) {
+            bars.removeClass('fa-times')
+            bars.addClass('fa-bars')
+            console.log(true)
+        } else {
+            bars.removeClass('fa-bars')
+            bars.addClass('fa-times')
+            console.log(true)
+        }
+    }, 100)
+})
