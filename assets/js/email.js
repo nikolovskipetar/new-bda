f2628fb9feaf5c6a70f525b48018c3d9a217a356
nikolovskipetar <petarnikolovski@hotmail.com>
2020-04-01 18:00:16 +0200

    test

--- assets/js/email.js
+++ assets/js/email.js
@@ -1,153 +1,118 @@
-/******************************************************************************
- * This tutorial is based on the work of Martin Hawksey twitter.com/mhawksey  *
- * But has been simplified and cleaned up to make it more beginner friendly   *
- * All credit still goes to Martin and any issues/complaints/questions to me. *
- ******************************************************************************/
-
-// if you want to store your email server-side (hidden), uncomment the next line
-// var TO_ADDRESS = "example@email.net";
-
-// spit out all the keys/values from the form in HTML for email
-// uses an array of keys if provided or the object to determine field order
-function formatMailBody(obj, order) {
-  var result = "";
-  if (!order) {
-    order = Object.keys(obj);
+(function() {
+  function validEmail(email) {
+    var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+    return re.test(email);
   }
 
-  // loop over all keys in the ordered form data
-  for (var idx in order) {
-    var key = order[idx];
-    result += "<h4 style='text-transform: capitalize; margin-bottom: 0'>" + key + "</h4><div>" + sanitizeInput(obj[key]) + "</div>";
-    // for every key, concatenate an `<h4 />`/`<div />` pairing of the key name and its value, 
-    // and append it to the `result` string created at the start.
-  }
-  return result; // once the looping is done, `result` will be one long string to put in the email body
-}
-
-// sanitize content from the user - trust no one 
-// ref: https://developers.google.com/apps-script/reference/html/html-output#appendUntrusted(String)
-function sanitizeInput(rawInput) {
-  var placeholder = HtmlService.createHtmlOutput(" ");
-  placeholder.appendUntrusted(rawInput);
-
-  return placeholder.getContent();
-}
-
-function doPost(e) {
-
-  try {
-    Logger.log(e); // the Google Script version of console.log see: Class Logger
-    record_data(e);
-
-    // shorter name for form data
-    var mailData = e.parameters;
-
-    // names and order of form elements (if set)
-    var orderParameter = e.parameters.formDataNameOrder;
-    var dataOrder;
-    if (orderParameter) {
-      dataOrder = JSON.parse(orderParameter);
+  function validateHuman(honeypot) {
+    if (honeypot) {  //if hidden form filled up
+      return true;
     }
-
-    // determine recepient of the email
-    // if you have your email uncommented above, it uses that `TO_ADDRESS`
-    // otherwise, it defaults to the email provided by the form's data attribute
-    var sendEmailTo = (typeof TO_ADDRESS !== "undefined") ? TO_ADDRESS : mailData.formGoogleSendEmail;
-
-    // send email if to address is set
-    if (sendEmailTo) {
-      MailApp.sendEmail({
-        to: String(sendEmailTo),
-        subject: "Contact form submitted",
-        // replyTo: String(mailData.email), // This is optional and reliant on your form actually collecting a field named `email`
-        htmlBody: formatMailBody(mailData, dataOrder)
-      });
-    }
-
-    return ContentService    // return json success results
-      .createTextOutput(
-        JSON.stringify({"result":"success",
-          "data": JSON.stringify(e.parameters) }))
-      .setMimeType(ContentService.MimeType.JSON);
-  } catch(error) { // if error return this
-    Logger.log(error);
-    return ContentService
-      .createTextOutput(JSON.stringify({"result":"error", "error": error}))
-      .setMimeType(ContentService.MimeType.JSON);
   }
-}
 
+  // get all data in form and return object
+  function getFormData(form) {
+    var elements = form.elements;
 
-/**
- * record_data inserts the data received from the html form submission
- * e is the data received from the POST
- */
-function record_data(e) {
-  var lock = LockService.getDocumentLock();
-  lock.waitLock(30000); // hold off up to 30 sec to avoid concurrent writing
-
-  try {
-    Logger.log(JSON.stringify(e)); // log the POST data in case we need to debug it
-
-    // select the 'responses' sheet by default
-    var doc = SpreadsheetApp.getActiveSpreadsheet();
-    var sheetName = e.parameters.formGoogleSheetName || "responses";
-    var sheet = doc.getSheetByName(sheetName);
-
-    var oldHeader = sheet.getRange(1, 1, 1, sheet.getLastColumn()).getValues()[0];
-    var newHeader = oldHeader.slice();
-    var fieldsFromForm = getDataColumns(e.parameters);
-    var row = [new Date()]; // first element in the row should always be a timestamp
-
-    // loop through the header columns
-    for (var i = 1; i < oldHeader.length; i++) { // start at 1 to avoid Timestamp column
-      var field = oldHeader[i];
-      var output = getFieldFromData(field, e.parameters);
-      row.push(output);
-
-      // mark as stored by removing from form fields
-      var formIndex = fieldsFromForm.indexOf(field);
-      if (formIndex > -1) {
-        fieldsFromForm.splice(formIndex, 1);
+    var fields = Object.keys(elements).filter(function(k) {
+      return (elements[k].name !== "honeypot");
+    }).map(function(k) {
+      if(elements[k].name !== undefined) {
+        return elements[k].name;
+        // special case for Edge's html collection
+      }else if(elements[k].length > 0){
+        return elements[k].item(0).name;
       }
+    }).filter(function(item, pos, self) {
+      return self.indexOf(item) == pos && item;
+    });
+
+    var formData = {};
+    fields.forEach(function(name){
+      var element = elements[name];
+
+      // singular form elements just have one value
+      formData[name] = element.value;
+
+      // when our element has multiple items, get their values
+      if (element.length) {
+        var data = [];
+        for (var i = 0; i < element.length; i++) {
+          var item = element.item(i);
+          if (item.checked || item.selected) {
+            data.push(item.value);
+          }
+        }
+        formData[name] = data.join(', ');
+      }
+    });
+
+    // add form-specific values into the data
+    formData.formDataNameOrder = JSON.stringify(fields);
+    formData.formGoogleSheetName = form.dataset.sheet || "responses"; // default sheet name
+    formData.formGoogleSendEmail = form.dataset.email || ""; // no email by default
+    return formData;
+  }
+
+  function handleFormSubmit(event) {  // handles form submit without any jquery
+    event.preventDefault();           // we are submitting via xhr below
+    var form = event.target;
+    var data = getFormData(form);         // get the values submitted in the form
+
+    /* OPTION: Remove this comment to enable SPAM prevention, see README.md
+    if (validateHuman(data.honeypot)) {  //if form is filled, form will not be submitted
+      return false;
     }
+    */
 
-    // set any new fields in our form
-    for (var i = 0; i < fieldsFromForm.length; i++) {
-      var field = fieldsFromForm[i];
-      var output = getFieldFromData(field, e.parameters);
-      row.push(output);
-      newHeader.push(field);
-    }
-
-    // more efficient to set values as [][] array than individually
-    var nextRow = sheet.getLastRow() + 1; // get next row
-    sheet.getRange(nextRow, 1, 1, row.length).setValues([row]);
-
-    // update header row with any new data
-    if (newHeader.length > oldHeader.length) {
-      sheet.getRange(1, 1, 1, newHeader.length).setValues([newHeader]);
+    if( data.email && !validEmail(data.email) ) {   // if email is not valid show error
+      var invalidEmail = form.querySelector(".email-invalid");
+      if (invalidEmail) {
+        invalidEmail.style.display = "block";
+        return false;
+      }
+    } else {
+      disableAllButtons(form);
+      $('#hidden-spinner').show();
+      var url = form.action;
+      var xhr = new XMLHttpRequest();
+      xhr.open('POST', url);
+      // xhr.withCredentials = true;
+      xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+      xhr.onreadystatechange = function() {
+        var formElements = form.querySelector(".form-elements");
+        var formSelector = document.getElementById("contact-column");
+        if (formElements) {
+          formElements.style.display = "none"; // hide form
+        }
+        var thankYouMessage = form.querySelector(".thankyou_message");
+        if (thankYouMessage) {
+          $(thankYouMessage).fadeIn();
+          $(thankYouMessage).css({"background-color": "#8167a9", "color": "white", "margin-bottom": "100px", "padding": "15px", "border-radius" : "12px"})
+        }
+        return;
+      };
+      // url encode form data for sending as post data
+      var encoded = Object.keys(data).map(function(k) {
+        return encodeURIComponent(k) + "=" + encodeURIComponent(data[k]);
+      }).join('&');
+      xhr.send(encoded);
     }
   }
-  catch(error) {
-    Logger.log(error);
+
+  function loaded() {
+    // bind to the submit event of our form
+    var forms = document.querySelectorAll("form.gform");
+    for (var i = 0; i < forms.length; i++) {
+      forms[i].addEventListener("submit", handleFormSubmit, false);
+    }
+  };
+  document.addEventListener("DOMContentLoaded", loaded, false);
+
+  function disableAllButtons(form) {
+    var buttons = form.querySelectorAll("button");
+    for (var i = 0; i < buttons.length; i++) {
+      buttons[i].disabled = true;
+    }
   }
-  finally {
-    lock.releaseLock();
-    return;
-  }
-
-}
-
-function getDataColumns(data) {
-  return Object.keys(data).filter(function(column) {
-    return !(column === 'formDataNameOrder' || column === 'formGoogleSheetName' || column === 'formGoogleSendEmail' || column === 'honeypot');
-  });
-}
-
-function getFieldFromData(field, data) {
-  var values = data[field] || '';
-  var output = values.join ? values.join(', ') : values;
-  return output;
-}
+})();
